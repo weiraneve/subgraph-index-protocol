f008b004248b5d9c7cae0b4ee96bc226a8b7aadb
weiraneve <907221539@qq.com>
2024-10-24 17:51:35 +0800

    refactor: extract handle common deposited logic

--- src/mappings/deposited.ts
+++ src/mappings/deposited.ts
@@ -5,52 +5,35 @@ import {
   Deposit,
   TotalDeposit
 } from "../../generated/schema"
-
 import { BigInt } from "@graphprotocol/graph-ts"
 import { JUNIOR_TRANCHE, SENIOR_TRANCHE } from '../utils/constants'
 
-export function handleJuniorLiquidityDeposited(event: LiquidityDepositedEvent): void {
+function handleLiquidityDeposited(event: LiquidityDepositedEvent, poolType: string): void {
   let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
-
   let deposit = new Deposit(id)
   deposit.lender = event.params.sender
   deposit.amount = event.params.assets
   deposit.shares = event.params.shares
   deposit.timestamp = event.block.timestamp
-  deposit.poolType = JUNIOR_TRANCHE
+  deposit.poolType = poolType
   deposit.blockNumber = event.block.number
   deposit.transactionHash = event.transaction.hash
   deposit.save()
 
-  let total = TotalDeposit.load(JUNIOR_TRANCHE)
+  let total = TotalDeposit.load(poolType)
   if (!total) {
-    total = new TotalDeposit(JUNIOR_TRANCHE)
+    total = new TotalDeposit(poolType)
     total.totalAmount = BigInt.fromI32(0)
-    total.poolType = JUNIOR_TRANCHE
+    total.poolType = poolType
   }
   total.totalAmount = total.totalAmount.plus(deposit.amount)
   total.save()
 }
 
+export function handleJuniorLiquidityDeposited(event: LiquidityDepositedEvent): void {
+  handleLiquidityDeposited(event, JUNIOR_TRANCHE)
+}
+
 export function handleSeniorLiquidityDeposited(event: LiquidityDepositedEvent): void {
-  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
-
-  let deposit = new Deposit(id)
-  deposit.lender = event.params.sender
-  deposit.amount = event.params.assets
-  deposit.shares = event.params.shares
-  deposit.timestamp = event.block.timestamp
-  deposit.poolType = SENIOR_TRANCHE
-  deposit.blockNumber = event.block.number
-  deposit.transactionHash = event.transaction.hash
-  deposit.save()
-
-  let total = TotalDeposit.load(SENIOR_TRANCHE)
-  if (!total) {
-    total = new TotalDeposit(SENIOR_TRANCHE)
-    total.totalAmount = BigInt.fromI32(0)
-    total.poolType = SENIOR_TRANCHE
-  }
-  total.totalAmount = total.totalAmount.plus(deposit.amount)
-  total.save()
+  handleLiquidityDeposited(event, SENIOR_TRANCHE)
 }
